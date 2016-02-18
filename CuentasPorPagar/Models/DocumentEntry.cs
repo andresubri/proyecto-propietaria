b88a18a7f229ddd5e780ea4fd12d0ce63b171778
Carlos Andres <andriusic@gmail.com>
2016-02-18 13:27:18 -0800

    -Added DocumentEntry model -Added PaymentConcept model

---
+++ CuentasPorPagar/Models/DocumentEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentasPorPagar.Models
+{
+    class DocumentEntry : Parse.ParseObject
+    {
+        public DocumentEntry(int documentNumber, int receiptNumber, string documentDate, int amount, string registerDate, string supplier, bool state)
+        {
+            DocumentNumber = documentNumber;
+            ReceiptNumber = receiptNumber;
+            DocumentDate = documentDate;
+            Amount = amount;
+            RegisterDate = registerDate;
+            Supplier = supplier;
+            this.state = state;
+        }
+
+        public int DocumentNumber { get; set; }
+        public int ReceiptNumber { get; set; }
+        public string DocumentDate { get; set; }
+        public int Amount { get; set; }
+        public string RegisterDate { get; set; }
+        public string Supplier { get; set; }
+        public bool state { get; set; }
+    }
+}
---
+++ CuentasPorPagar/Models/PaymentConcept.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentasPorPagar
+{
+    class PaymentConcept : Parse.ParseObject
+    {
+        public PaymentConcept(string id, string description, bool state)
+        {
+            Id = id;
+            Description = description;
+            State = state;
+        }
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public bool State { get; set; }
+    }
+}

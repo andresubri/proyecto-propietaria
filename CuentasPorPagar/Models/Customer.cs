57457887fc9a73cf4066ec5d5df7ba1a56971fe7
Carlos Andres <andriusic@gmail.com>
2016-02-18 13:48:11 -0800

    -Added User model

---
+++ CuentasPorPagar/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentasPorPagar.Models
+{
+    class Customer : Parse.ParseObject
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+
+
+    }
+}

49fd4d76f40f26efca69244fdec0dc062f7e9394
Carlos Andres <andriusic@gmail.com>
2016-02-19 03:54:44 -0800

    -Added some CRUD views to the Views directory -Editted PaymentConcept model

--- CuentasPorPagar/Models/PaymentConcept.cs
+++ CuentasPorPagar/Models/PaymentConcept.cs
@@ -8,14 +8,19 @@ namespace CuentasPorPagar
 {
     class PaymentConcept : Parse.ParseObject
     {
-        public PaymentConcept(string id, string description, bool state)
+        public PaymentConcept(string id, string description, int monto, string supplier, bool state)
         {
             Id = id;
             Description = description;
+            Monto = monto;
+            Supplier = supplier;
             State = state;
         }
+
         public string Id { get; set; }
         public string Description { get; set; }
+        public int Monto { get; set; }
+        public string Supplier { get; set; }
         public bool State { get; set; }
     }
 }
